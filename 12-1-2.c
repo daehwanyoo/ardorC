65ccdf540ef4703a56c3c459a596a634cfb9bb49
Daehwan Yoo <ydh05051@gmail.com>
2021-02-02 22:04:49 -0500

    created 12-1-2

---
+++ 12-1-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 10, num2 = 20;
+	int* ptr1=&num1;
+	int* ptr2=&num2;
+	int* temp;
+
+	*ptr1 += 10;
+	*ptr2 -= 10;
+
+	temp = ptr1;
+	ptr1 = ptr2;
+	ptr2 = temp;
+
+	printf("The ptr1 points to %d and ptr2 points to %d", *ptr1, *ptr2);
+
+
+	return 0;
+}
