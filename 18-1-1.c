7346b024fcf99443556e7263b296750b9fc486bd
Daehwan Yoo <ydh05051@gmail.com>
2021-02-04 18:09:55 -0500

    Add 18-1-1.c

---
+++ 18-1-1.c
@@ -0,0 +1,21 @@
+//==========================================================
+//
+// Title:      18-1-1
+// Author:     Daehwan Yoo
+// Date:       Feb-4-2020
+// Description:
+//   Guess the same pointer 
+// 
+//==========================================================
+#include <stdio.h>
+ 
+int main(void)
+{
+	int* arr1[5];
+	int* arr2[3][5];
+
+	int** a1 = arr1;
+	int*(*a2)[5] = arr2;
+
+	return 0;
+}
